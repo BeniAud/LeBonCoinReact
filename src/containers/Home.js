ea1a1045d32f140cc275c6cd5d0e766159b8523d
zizou <zizouvb@users.noreply.github.com>
2018-11-22 12:06:36 +0100

    add filter search

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class Home extends React.Component {
   render() {
-    return <h1>Home</h1>;
+    return <h1>{this.props.history.push("/offers/")}</h1>;
   }
 }
 
--- src/containers/Offers/Offers.js
+++ src/containers/Offers/Offers.js
@@ -3,7 +3,13 @@ import axios from "axios";
 import "./styles.css";
 class Offer extends React.Component {
   state = {
-    listItems: []
+    listItems: [],
+    title: "",
+    priceMin: "",
+    priceMax: "",
+    sort: "",
+    skip: "",
+    limit: ""
   };
 
   displayItems = () => {
@@ -26,8 +32,81 @@ class Offer extends React.Component {
       );
     });
   };
+  handleChange = event => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    console.log(target.type, name, value);
+    this.setState({ [name]: value });
+    if (target.type === "select-one") {
+      this.searchFilters(event);
+    }
+  };
+  searchFilters = event => {
+    event.preventDefault();
+    axios
+      .get("https://leboncoin-api.herokuapp.com/api/offer/", {
+        params: {
+          title: this.state.title,
+          priceMin: this.state.priceMin,
+          priceMax: this.state.priceMax,
+          sort: this.state.sort,
+          skip: this.state.skip,
+          limit: this.state.limit
+        }
+      })
+      .then(response => {
+        this.setState({ listItems: response.data });
+        console.log(response.data);
+      });
+  };
   render() {
-    return <div className="container-offers">{this.displayItems()}</div>;
+    return (
+      <div>
+        <div>
+          <form onSubmit={this.searchFilters}>
+            <div>
+              <input
+                placeholder="Que recherchez vous?"
+                id="title"
+                type="text"
+                name="title"
+                onChange={this.handleChange}
+              />
+              <button>Rechercher</button>
+            </div>
+            <div>
+              <label htmlFor="priceMin">Prix entre</label>
+              <input
+                placeholder="prix min"
+                id="priceMin"
+                type="number"
+                name="priceMin"
+                onChange={this.handleChange}
+              />
+              <label htmlFor="priceMin">et</label>
+              <input
+                placeholder="prix max"
+                id="priceMax"
+                type="number"
+                name="priceMax"
+                onChange={this.handleChange}
+              />
+
+              <select onChange={this.handleChange} name="sort">
+                <option selected value="date-desc">
+                  Plus récentes
+                </option>
+                <option value="date-asc">Plus anciennes</option>
+                <option value="price-asc">Moins chères</option>
+                <option value="price-desc">Plus chères</option>
+              </select>
+            </div>
+          </form>
+        </div>
+        <div className="container-offers">{this.displayItems()}</div>
+      </div>
+    );
   }
 
   componentDidMount() {
